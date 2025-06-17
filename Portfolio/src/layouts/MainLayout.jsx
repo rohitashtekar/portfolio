f9706ca3dea52831310ca0e157ec27d4fbde7ac8
Rohit Ashtekar <68610103+rohitashtekar@users.noreply.github.com>
2025-06-17 11:00:42 +0530

    ADDED: Project initialisation, dependencies

---
+++ Portfolio/src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar.jsx'
+import Footer from '../components/Footer.jsx'
+
+const MainLayout = () => {
+  return (
+    <>
+        <Navbar/>
+        <Outlet/>
+        <Footer/>
+    </>
+  )
+}
+
+export default MainLayout
