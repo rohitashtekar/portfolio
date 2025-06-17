ac3904ac122d6a843b4fc25f369933fbf0b68bd9
Rohit Ashtekar <68610103+rohitashtekar@users.noreply.github.com>
2025-06-17 11:02:07 +0530

    ADDED: initialised home page

--- Portfolio/src/pages/Home.jsx
+++ Portfolio/src/pages/Home.jsx
@@ -1,6 +1,14 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className="wc-container">
+        <h1>Welcome.</h1>
+      </div>
+
+      <div className="main-container container-fluid">
+
+      </div>
+    </>
   )
 }
 
