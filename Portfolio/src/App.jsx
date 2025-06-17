9db4b8195478cc3fab49410b3408a36fd2d38957
Rohit Ashtekar <68610103+rohitashtekar@users.noreply.github.com>
2025-06-17 11:01:03 +0530

    ADDED: routes, imports

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,12 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/variables.css'
+import MainLayout from './layouts/MainLayout';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Terms from './pages/Terms';
 
 function App() {
 
   return (
     <>
-      <div className='main-container'>
-        
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout/>}>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/projects' element={<Projects/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/terms' element={<Terms/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
