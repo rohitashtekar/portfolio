8f4d59545e4098340bfc1a61bf5e966feda29de4
Rohit Ashtekar <68610103+rohitashtekar@users.noreply.github.com>
2025-06-19 04:34:30 +0530

    UPDATE: removed unwanted code

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -3,20 +3,14 @@ import { Link } from "react-router-dom"
 const Footer = () => {
   return (
     <>
-        <footer className="bg-dark text-white pt-2">
-            <div className="container-fluid d-sm-flex justify-content-between footer-container">
-                <span>© Rohit Ashtekar</span>
-                <div className="d-flex justify-content-center  terms-policy">
-                    
-                    <Link className="terms" to="/terms">
-                    <p className="mx-3">Terms of Use</p>
-                    </Link>
-                    
-                    <span className="mx-3">Cookie Policy</span>
-                    <span className="mx-3">Privacy Policy</span>  
-                </div>
-            </div>
-        </footer>
+      <footer className="bg-dark text-white p-2">
+          <div className="container-fluid d-sm-flex justify-content-between footer-container">
+              <span>© Rohit Ashtekar</span>
+              <div className="d-flex justify-content-center  terms-policy">
+
+              </div>
+          </div>
+      </footer>
     </>
   )
 }
