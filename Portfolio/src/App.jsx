7fc4bcaeb0e83d024539214de9b6c0d6ceef58d6
Rohit Ashtekar <68610103+rohitashtekar@users.noreply.github.com>
2025-07-08 01:49:47 +0530

    UPDATE: added routes, imports

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,26 +1,40 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import './styles/variables.css'
+import './styles/variables.css';
+import './styles/global.css';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+import SecondClinic from './projects/SCApp';
+import Chatbot from './projects/Chatbot';
+import CoffeeOrder from './projects/CoffeeOrder';
 import About from './pages/About';
 import Terms from './pages/Terms';
+import Skills from './pages/Skills';
+import ScrollToTop from './utils/ScrollToTop';
+import Resume from './components/Resume';
 
 function App() {
 
   return (
-    <>
+    <>  
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
-            <Route element={<MainLayout/>}>
-              <Route path='/' element={<Home/>}/>
-              <Route path='/projects' element={<Projects/>}/>
-              <Route path='/about' element={<About/>}/>
-              <Route path='/contact' element={<Contact/>}/>
-              <Route path='/terms' element={<Terms/>}/>
-            </Route>
+          <Route element={<MainLayout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/skills' element={<Skills />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/terms' element={<Terms />} />
+
+            {/* Projects */}
+            <Route path='/projects/secondclinic' element={<SecondClinic />} />
+            <Route path='/projects/chatbot' element={<Chatbot />} />
+            <Route path='/projects/coffeeorder' element={<CoffeeOrder />} />
+
+            {/* Resume */}
+            <Route path='/resume' element={<Resume />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
