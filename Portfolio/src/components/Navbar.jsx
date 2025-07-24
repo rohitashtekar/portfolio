680ccc89a26e83517ff1f4c216f21cff43d89d18
Rohit Ashtekar <68610103+rohitashtekar@users.noreply.github.com>
2025-07-24 20:23:28 +0530

    UPDATE: div alignment

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -13,13 +13,13 @@ const Navbar = ({ isDark, setIsDark }) => {
         <>
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <Link to="/">
-                        <img className="nav-logo" src={ isDark? brandLight: brandDark } alt="Logo" />
-                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <Link to="/" className="logo-link">
+                        <img className="nav-logo" src={ isDark? brandLight: brandDark } alt="Logo" />
+                    </Link>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <Link className="nav-link" aria-current="page" to="/">HOME</Link>
@@ -33,10 +33,12 @@ const Navbar = ({ isDark, setIsDark }) => {
                             <li className="nav-item">
                                 <Link className="nav-link" aria-current="page" to="/about">ABOUT</Link>
                             </li>
+                            <li>
+                            </li>
                         </ul>
-                    <div className="disp-mode ms-3" onClick={ toggleTheme }>
-                        <i className="fa-solid fa-circle-half-stroke"></i>
-                    </div>
+                        <div className="disp-mode" onClick={ toggleTheme }>
+                            <i className="fa-solid fa-circle-half-stroke"></i>
+                        </div>
                     </div>
                     {/* <div className="d-flex ms-3" role="email">
                         <button className="btn btn-outline-light" type="submit">rrashtekar@gmail.com</button>
