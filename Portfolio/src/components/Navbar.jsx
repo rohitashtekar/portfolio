af15e0bc2f6ab64e784dae29892a5f6cfc72dc90
Rohit Ashtekar <68610103+rohitashtekar@users.noreply.github.com>
2025-11-04 15:16:32 +0530

    UPDATE: logo postion reponsive design

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -13,12 +13,12 @@ const Navbar = ({ isDark, setIsDark }) => {
         <>
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
                     <Link to="/" className="logo-link">
                         <img className="nav-logo" src={ isDark? brandLight: brandDark } alt="Logo" />
                     </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
                     <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                         <ul className="navbar-nav">
                             <li className="nav-item">
