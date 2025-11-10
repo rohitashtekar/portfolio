eb5f351dce9baa9b262a1a427372ec21d7455bde
Rohit Ashtekar <68610103+rohitashtekar@users.noreply.github.com>
2025-11-10 17:33:58 +0530

    UPDATE: section className

--- Portfolio/src/pages/Home.jsx
+++ Portfolio/src/pages/Home.jsx
@@ -93,7 +93,7 @@ const Home = () => {
         </div>
       </section>
 
-      <section data-section="teen">
+      <section className="container-3" data-section="teen">
         <Work />
       </section>
     </>
