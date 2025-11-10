a7e19ea7ca5bd4ca52306f8a556907ca958e7b75
Rohit Ashtekar <68610103+rohitashtekar@users.noreply.github.com>
2025-11-10 13:24:25 +0530

    ADDED: ScrollDpwn animation

---
+++ Portfolio/src/components/ScrollDown.jsx
@@ -0,0 +1,11 @@
+import '../styles/components/scrolldown.css';
+
+const ScrollDown = () => {
+    return (
+        <>
+            <div className="scroll-down"></div>
+        </>
+    )
+}
+
+export default ScrollDown
